895e1fe8cb5a9faa7de69b3336af2fbb91bae62f
Cole <80992301+Duststorm741@users.noreply.github.com>
2023-10-26 21:01:34 -0400

    Update ProjectServer.java

--- src/main/java/com/mycompany/projectserver/ProjectServer.java
+++ src/main/java/com/mycompany/projectserver/ProjectServer.java
@@ -1,43 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.projectserver;
-
 import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/**
- *
- * @author server
- */
 public class ProjectServer {
-    
+
     public static void main(String[] args) throws IOException {
         System.out.println("Server port is 5678");
-        
-        String msg;
+
+        // Create a server socket listening on port 5678
         ServerSocket ss = new ServerSocket(5678);
-        
-        while(true)
-        {
+
+        while (true) {
+            // Accept incoming client connections
             Socket s = ss.accept();
-            
+
+            // Create a DataInputStream to receive data from the client
             DataInputStream dis = new DataInputStream(s.getInputStream());
-            msg = dis.readUTF();
-            
-            
-            dis.close();
-            s.close();
-            System.out.println("Recieved data: " + msg);
-                 
-            
+
+            try {
+                // Read the data sent by the client as a UTF-encoded string
+                String msg = dis.readUTF();
+                System.out.println("Received data: " + msg);
+            } catch (EOFException e) {
+                // Handle the end of the stream (e.g., client disconnected)
+                System.out.println("Client disconnected.");
+            } catch (IOException e) {
+                // Handle other IO errors
+                e.printStackTrace();
+            } finally {
+                // Close the DataInputStream and the client socket
+                dis.close();
+                s.close();
+            }
         }
-        
-        
-        
     }
-    
 }
